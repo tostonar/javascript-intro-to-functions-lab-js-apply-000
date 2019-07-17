62f5463ee39a70bf06a95a2481d86e13bbf95768
Andrea Toston <tostonar@gmail.com>
2019-07-17 14:06:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,7 @@ function sayHiToGrandma(string) {
      return "I can't hear you!";
    } else if (string === string.toUpperCase()) {
      return "YES INDEED!";
+   } else if (string === "I love you, Grandema.") {
+     return "I love you, too."
    }
 }
